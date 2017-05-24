689436efca01f6fca877357c086c2cc0912160c3
Richi <ricardo.pablito@yahoo.de>
2017-05-24 18:31:28 +0200

    error fixing 3

--- KMeans.java
+++ KMeans.java
@@ -6,9 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.IllegalArgumentException;
 
-import java.lang.IllegalArgumentException;
-import 
-
 public class KMeans {
 
     private Double hashFuncs[][] = 
