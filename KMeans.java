78916aa3e84c67d12c1787a96ac9b8913d97e64b
Richi <ricardo.pablito@yahoo.de>
2017-05-24 18:06:27 +0200

    centroid initialisation in the algorithm-function finished

--- KMeans.java
+++ KMeans.java
@@ -1,6 +1,20 @@
 
 public class KMeans {
 
+    private Double hashFuncs[][] = 
+        {
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+            {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}
+            {0, 0, 1, 0, 0, 0, 0, 0, 0, 0}
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0}
+            {0, 0, 0, 0, 1, 0, 0, 0, 0, 0}
+            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0}
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 0}
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0}
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+        };
+
 	public static void main(String[] args) {
 
 		HashMap<String, Object> config = readArgs(args);
@@ -23,8 +37,49 @@ public class KMeans {
 	 */
 	private List<Integer>[][] hash(Double[][] points) {
 	}
+	
+	/**
+	 *  calculates the hash value of a point and a hash function as
+	 *  a vector-vector-product
+	 */
+	private Integer calcHash (Double point[], Double func[]) {
+	    if (point.length != func.length) {
+	        throw IllegalArgumentException("vector dimensions have to match!");
+        }
+        
+	    Integer sum;
+	    for (int i = 0; i < point.length; ++i) {
+	        sum += (point[i] * func[i]);
+	    }
+	    
+	    return sum;
+	}
 
 	private void algorithmus(Double[][] points, List<Integer>[][] buckets) {
+	
+	    int clusters = 15;
+	    int dimension = 10;
+	    int max = points.length;
+	    
+	    Double centroids[clusters][dimension];
+	    
+	    int randomNum;
+	    
+	    for (int i = 0; i < clusters; ++i) {
+	        // take a point at a random index 
+	        // position from points and use it as centroid
+            randomNum = rand.nextInt(max + 1);
+            centroids[i] = points[randomNum];
+        }
+        
+        Double cHashValues[clusters][dimension];
+        
+        // calculate the hash value for every centroid for every hash function
+        for (int i = 0; i < clusters; ++i) {
+            for (int j = 0; j < hashFuncs.length; ++j) {
+                cHashValues[i][j] = calcHash (centroids[i], hashFuncs[j]);
+            }
+        }
 	}
 
 	private Double distance(Double[] a, Double[] b) {
